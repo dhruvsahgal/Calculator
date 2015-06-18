0f348562dcde9b11a7b88a6ff14f226c04daf587
dhruvsahgal <dhruvsahgal@gmail.com>
2015-06-18 19:22:25 +0800

    Intiial commit of basic calculator

---
+++ app.js
@@ -0,0 +1,53 @@
+var values = [];
+var screenVal = 0;
+
+var recieveNum = function(val){
+    if (values.length === 0){
+    values[0] = val;
+    console.log(values[0]);
+    screenVal = val;
+    document.getElementById("screen").innerHTML = screenVal;
+
+    } else if (values.length === 1){
+    values[1] = val;
+    console.log(values[1]);
+    screenVal = val;
+    document.getElementById("screen").innerHTML = screenVal;
+    };
+    console.log(values);
+};
+
+var recieveOp = function(op){
+    operator = op;
+    switch(operator){
+        case 1:
+            screenVal = values[0] + values[1];
+            document.getElementById("screen").innerHTML = screenVal;
+            console.log(screenVal);
+            break;
+        case 2:
+             screenVal = values[0] - values[1];
+            document.getElementById("screen").innerHTML = screenVal;
+            console.log(screenVal);
+            break;
+        case 3:
+            screenVal = values[0] * values[1];
+            document.getElementById("screen").innerHTML = screenVal;
+            console.log(screenVal);
+            break;
+        case 4:
+             screenVal = values[0] / values[1];
+            document.getElementById("screen").innerHTML = screenVal;
+            console.log(screenVal);
+            break;   
+    };
+    console.log("Operator val = " + operator);      
+};  
+
+
+
+
+
+var reset = function(){
+    location.reload();
+};
