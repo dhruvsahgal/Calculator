a0cfb1474f897d429a653df0263fee02326e9e30
dhruvsahgal <dhruvsahgal@gmail.com>
2015-06-26 11:14:20 +0800

    Added comments because that's good practice

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var values = [];
-var conVal ;
+var conVal;
 var screenVal = 0;
 
-var input = function(value){
-    values.push(value);
+var input = function (value) {
+    values.push(value); //Moves incoming values into an array
     console.log(values);
     screenVal = value;
-    document.getElementById("screen").innerHTML = screenVal;
+    document.getElementById("screen").innerHTML = screenVal; //Displays the latest value pressed
 };
 
-var equals = function(){
-    conVal = values.join("");
-    var a = eval(conVal);
+var equals = function () {
+    conVal = values.join(""); // Joins the array into a single string
+    var a = eval(conVal); //method evaluats a string as js
     console.log(conVal);
     console.log(a);
-    document.getElementById("screen").innerHTML = a;
-    values.length = 0;
+    document.getElementById("screen").innerHTML = a; //puts final answer into the screen of the calculator
+    values.length = 0; //clears the array
 
 };
-var clear = function(){
+var clear = function () {
     location.reload();
 };
 
